0d7f958875c2990c21894b441a668e9ef83aa6c3
Bobby Graese <bobby@grae.se>
2015-02-21 11:12:12 -0600

    First test: 'SimpleParent'
    A framework for making switching parents easier in C++.  I got tired of referencing the base class by name, and wished I had something like Python's 'self'.  This aims to accomplish that.  Then, if you want to switch parents, you should only need to change on place in code.

---
+++ simple-parent/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+template<typename T>
+class SimpleParent : public T {
+public:
+    template<typename... Args>
+    SimpleParent(Args... values) : T(values...) { }
+    virtual ~SimpleParent() = default;
+protected:
+    typedef T Parent;
+};
+
+class Base {
+public:
+    Base(const std::string& s) { std::cout << "Base::Base(" << s << ")" << std::endl; }
+    virtual ~Base() = default;
+
+    virtual void m1() { std::cout << "Base::m1" << std::endl; }
+    virtual void m2() { std::cout << "Base::m2" << std::endl; }
+};
+
+class Child1 : public SimpleParent<Base>
+{
+public:
+    Child1(std::string name) : SimpleParent<Base>(name) { }
+    virtual ~Child1() = default;
+
+    virtual void m1() { std::cout << "Child1::m1" << std::endl; Parent::m1(); }
+    virtual void m2() { Parent::m2(); }
+};
+
+int main(int, char**) {
+    Child1 c1("something");
+    c1.m1();
+    Base& b = c1;
+    b.m1();
+    b.m2();
+
+    return 0;
+}
