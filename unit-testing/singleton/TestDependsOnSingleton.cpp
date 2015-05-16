d68bdb35ec2d2dcfe50f8e978add7f524dad8a33
Bobby Graese <bobby@grae.se>
2015-05-15 23:03:34 -0500

    Make the test cases a bit more involved. Really, I just wanted to play around with gtest/gmock a little bit :)

--- unit-testing/singleton/TestDependsOnSingleton.cpp
+++ unit-testing/singleton/TestDependsOnSingleton.cpp
@@ -8,8 +8,31 @@ TEST(DependencyTest, CanSetMemberVariable) {
 
     DependsOnSingleton dos { };
 
-    EXPECT_CALL(singleton, setMemberVariable(8))
+    EXPECT_CALL(singleton, setMemberVariable(7))
         .Times(1);
 
     dos.stupidRedirectSet(7);
+}
+
+TEST(DependencyTest, CanGetMemberVariable) {
+    SingletonAbstraction singleton { };
+    SingletonAbstraction::abstraction = &singleton;
+
+    DependsOnSingleton dos { };
+
+    std::vector<int> test_conditions = {
+        std::numeric_limits<int>::min(),
+        std::numeric_limits<int>::max(),
+        0, 10, -8
+    };
+
+    for (const auto& condition : test_conditions) {
+        EXPECT_CALL(singleton, setMemberVariable(condition))
+            .Times(1);
+        EXPECT_CALL(singleton, getMemberVariable())
+            .WillOnce(::testing::Return(condition));
+
+        dos.stupidRedirectSet(condition);
+        EXPECT_EQ(condition, dos.stupidRedirectGet());
+    }
 }
